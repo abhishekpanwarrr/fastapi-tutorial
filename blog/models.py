b396b2568f346037504d52d9a71d2e2f6f6a538e
Abhishek <40629087+abhishekpanwarrr@users.noreply.github.com>
2023-09-30 20:32:54 +0530

    models added for auth

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import Relationship
 
 
 class Blog(Base):
@@ -8,6 +9,9 @@ class Blog(Base):
     id = Column(Integer, primary_key=True, index=True, )
     title = Column(String)
     body = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    creator = Relationship("User", back_populates="blogs")
 
 
 class User(Base):
@@ -18,3 +22,4 @@ class User(Base):
     email = Column(String)
     password = Column(String)
 
+    blogs = Relationship("Blog", back_populates="creator")
